b3e64718fd27855955b8537bc80c01f25a999df1
shuttlesaifdev <saiful@shuttlebd.com>
2019-09-05 17:26:24 +0600

    Restify with mysql Rest Api

---
+++ config.js
@@ -0,0 +1,21 @@
+
+
+'use strict'
+ 
+var mysql = require('mysql');
+ 
+module.exports = {
+    name: 'rest-api',
+    hostname : 'http://localhost',
+    version: '0.0.1',
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    db: {
+        get : mysql.createConnection({
+		host     : 'localhost',
+		user     : 'shuttle',
+		password : '123456',
+		database : 'test'
+		})
+    }
+}
